c848b505b3298d5926a808d3eaabff08f60f3d0a
hassanuahmad <ahmad12p@uwindsor.ca>
2021-12-01 01:33:14 -0500

    2021 day 1 complete

---
+++ 2021/Day 1/p1.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+fs.readFile("input.txt", (err, data) => {
+    if (err) throw err;
+
+    const inputData = data.toString().replace(/\r\n/g, "\n").split("\n");
+    const numInputData = inputData.map((i) => Number(i));
+
+    let counter = 0;
+
+    for (let i = 0; i < numInputData.length; i++) {
+        if (numInputData[i + 1] > numInputData[i]) {
+            counter++;
+        }
+    }
+    console.log(counter);
+});
---
+++ 2021/Day 1/p2.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+
+fs.readFile("input.txt", (err, data) => {
+    if (err) throw err;
+
+    const inputData = data.toString().replace(/\r\n/g, "\n").split("\n");
+    const numInputData = inputData.map((i) => Number(i));
+
+    let counter = 0;
+
+    for (let i = 0; i < numInputData.length; i++) {
+        const com1 =
+            numInputData[i] + numInputData[i + 1] + numInputData[i + 2];
+
+        const com2 =
+            numInputData[i + 1] + numInputData[i + 2] + numInputData[i + 3];
+
+        if (com2 > com1) {
+            counter++;
+        }
+    }
+    console.log(counter);
+});
